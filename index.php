c9c4838a44ed4946de566935c2b008b55e2243d6
Nima Rahbar <nima-rahbar@users.noreply.github.com>
2022-11-21 12:31:16 +0200

    Update favicon

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+	<link rel="icon" type="image/x-icon" href="img/favicon.png">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
 	      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -30,7 +31,7 @@
 	<div class="row">
 		<nav id="nav-head" class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top d-print-none">
 			<a class="navbar-brand" href="https://nimarahbar.com">
-				<img class="img-thumbnail bg-dark" src="https://nimarahbar.com/wp-content/uploads/2020/12/favicon.png"
+				<img class="img-thumbnail bg-dark" src="img/favicon.png"
 				     alt="Nima Rahbar">
 				<span class="ms-2">Nima Rahbar</span>
 			</a>
